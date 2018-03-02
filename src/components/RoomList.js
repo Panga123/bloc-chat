d0d2c545fe57c7f2123a0694ccfa7d54e85bfca2
An Mo <ann@amurraypr.com>
2018-03-02 07:31:38 -0800

    Finalized checkpoint blocchat 2

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import '../App.css';
  console.log("testing")
 
-
 class RoomList extends Component { //  creating a roomlist class component, and export it.
   constructor(props) { //initialize to use the state object
     super(props);
@@ -15,19 +14,20 @@ class RoomList extends Component { //  creating a roomlist class component, and 
   render() { // using map to display data for each room
     return (
       <section className="RoomList">
-         {this.state.rooms.map ((room) => (  //to loop over the room array to render its contents
-           <h1>{room.key}</h1>
+      <ul>
+         {this.state.rooms.map ((room, i) => (  //to loop over the room array to render its contents Q: why state and not props?
+           <li key={room.key}>{room.name}</li>
          ))}
+     </ul>
       </section>
+
     );
   }
 
   componentDidMount() {
      this.roomsRef.on('child_added', snapshot => {
-       //console.log(snapshot);
        const room = snapshot.val();
-       room.key = snapshot.key; // question about key concept here
-       //this.setState({ rooms: this.state.rooms.concat( room ) })
+       room.key = snapshot.key; 
        this.setState({ rooms: this.state.rooms.concat( room ) })
 
      });
